f82cb491f1d850afbe19115d80aaf785fdaef0e5
michaworku <mikias27worku@gmail.com>
2022-03-19 12:32:44 +0300

    express project setup

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello Netflix clone API!</h1>');
+});
+
+app.listen(process.env.PORT, () => console.log(`Running on ${process.env.PORT} ⚡`));
