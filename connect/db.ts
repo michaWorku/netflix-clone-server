8f40ae49e7594a26d5185d75ac30a3bbcc366a32
michaworku <mikias27worku@gmail.com>
2022-03-19 13:35:26 +0300

    handling errors - uncaught exception - unhandled redjection

--- connect/db.ts
+++ connect/db.ts
@@ -1,6 +1,13 @@
+import log from '../logger'
+
+process.on('uncaughtException', (err: Error) => {
+    log.error('UNCAUGHT EXCEPTION! 🧨 SHUTTING DOWN!');
+    log.error(`${err}`);
+    process.exit(1);
+  });
+
 import mongoose from 'mongoose'
 import config from 'config'
-import log from '../logger'
 
 const connectDB = async () => {
   const DB_URI = config.get('LOCAL_DB_URI') as string
@@ -10,9 +17,16 @@ const connectDB = async () => {
 
         log.info(`MongoDB connected: ${conn.connection.host}`)
     }catch(err){
-        console.log(err)
+        log.error(err)
         process.exit(1)
     }
 }
+
+process.on('unhandledRejection', (err: Error) => {
+    log.error(`Name: ${err.name}`);
+    log.error(`Message: ${err.message}`);
+    log.error('UNHANDLED REJECTION! 🧨 SHUTTING DOWN!');
+    process.exit(1)
+});
 
 export default connectDB
