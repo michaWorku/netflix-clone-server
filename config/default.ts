337220af5df4b4b19ae0c5f2584dc15eb68f3912
michaworku <mikias27worku@gmail.com>
2022-03-19 13:26:01 +0300

    config env variables

---
+++ config/default.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export default {
+  PORT: process.env.PORT,
+  LOCAL_DB_URI: process.env.MONGO_LOCAL,
+  DB_URI: process.env.MONGODB_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES: process.env.JWT_EXPIRES
+}
